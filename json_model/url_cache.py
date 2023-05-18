e8bad9fad13b0fd46871ee17e210aeb7fa7e704d
Fabien Coelho <fabien@coelho.net>
2023-05-18 07:02:10 +0200

    add convenient url_cache thing

---
+++ json_model/url_cache.py
@@ -0,0 +1,45 @@
+import os
+import json
+import urllib
+import requests
+import hashlib
+
+class jsonURLCache:
+    """Cache JSON URL."""
+
+    def __init__(self, cache_dir: str = None):
+
+        # keep a local copy
+        self._cache_dir = cache_dir or (os.environ.get("HOME", ".") + "/.cache/json")
+        if not os.path.exists(self._cache_dir):
+            os.mkdir(self._cache_dir)
+        assert os.path.isdir(self._cache_dir)
+        
+        # class cache
+        self._cache = {}
+
+    def load(self, url: str):
+
+        up = urllib.parse.urlparse(url)
+        # FIXME add query?
+        u = up.scheme + "://" + up.netloc + up.path
+
+        # first cache (memory)
+        if u in self._cache:
+            return self._cache[u]
+
+        # second cache (file): we assume no hash collision!
+        h = hashlib.sha3_256(u.encode("UTF-8")).hexdigest()
+        hfile = self._cache_dir + "/" + h
+        if os.path.exists(hfile):
+            with open(hfile) as f:
+                j = json.load(f)
+                self._cache[u] = j
+                return j
+
+        # download
+        j = requests.get(u).json()
+        self._cache[u] = j
+        with open(hfile, "w") as f:
+            json.dump(j, f)
+        return j
