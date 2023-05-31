23db0b8745234ccee44a80abde96b471c210521d
Fabien Coelho <fabien@coelho.net>
2023-05-31 11:21:39 +0200

    export test function

--- json_model/__init__.py
+++ json_model/__init__.py
@@ -6,3 +6,4 @@ from json_model.c_check_model import c_check_model
 from json_model.v_check_model import v_check_model
 from json_model.schema2model import schema2model_script
 from json_model.model2schema import model2schema_script
+from json_model.merge import merge
--- json_model/merge.py
+++ json_model/merge.py
@@ -1,10 +1,13 @@
-#! /usr/bin/env python
+#
+# test merge operator
+#
 
-import json
 import sys
+import json
 from json_model.utils import merge_rewrite
 
-for fn in sys.argv[1:]:
-    with open(fn) as f:
-        data = json.load(f)
-        print(json.dumps(merge_rewrite(data), indent=2))
+def merge():
+    for fn in sys.argv[1:]:
+        with open(fn) as f:
+            data = json.load(f)
+            print(json.dumps(merge_rewrite(data), indent=2))
