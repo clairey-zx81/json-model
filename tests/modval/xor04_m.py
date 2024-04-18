a700c415717bb4e2b0663e2a43e4da1ef82a839d
Fabien Coelho <fabien@coelho.net>
2024-04-18 15:34:09 +0200

    add a simple example for the paper

---
+++ tests/modval/xor04_m.py
@@ -0,0 +1,26 @@
+import re
+from typing import Any, Callable
+
+CheckFun = Callable[[Any, str], bool]
+
+# regex "/^[A-Z][a-z]+$/"
+jmsc_re_0 = re.compile("^[A-Z][a-z]+$").search
+
+
+
+# define "" ($)
+def jmsc_f_0(value: Any, path: str) -> bool:
+    # $
+    # $.|[0]
+    result = isinstance(value, float) and value >= 0.0
+    if not result:
+        # $.|[1]
+        # "/^[A-Z][a-z]+$/"
+        result = isinstance(value, str) and jmsc_re_0(value) is not None
+    return result
+
+# object properties must and may maps
+
+
+def check_model(value) -> bool:
+    return jmsc_f_0(value, "$")
