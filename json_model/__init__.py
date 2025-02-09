972fd6ab6ea14c376829d6f1bd582875e7699a5e
Fabien Coelho <fabien@coelho.net>
2025-02-09 09:33:58 +0100

    full package is ruff ok

--- json_model/__init__.py
+++ json_model/__init__.py
@@ -2,8 +2,10 @@
 # JSON Model Scripts
 #
 
-from .preproc import preprocessor
-from .validator import v_check_model
-from .dynamic_compiler import c_check_model
-from .static_compiler import static_compiler, static_compiler_check
-from .convert import schema2model_script, model2schema_script
+from .preproc import preprocessor as preprocessor
+from .validator import v_check_model as v_check_model
+from .dynamic_compiler import c_check_model as c_check_model
+from .static_compiler import static_compiler as static_compiler
+from .static_compiler import static_compiler_check as static_compiler_check
+from .convert import schema2model_script as schema2model_script
+from .convert import model2schema_script as model2schema_script
