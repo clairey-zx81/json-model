bc34dcedc67f58edaab051f6668e7bc810c7ed7d
Fabien Coelho <fabien@coelho.net>
2025-04-25 10:23:24 +0200

    schema to model conversion moved to json-schema-utils

--- json_model/__init__.py
+++ json_model/__init__.py
@@ -1,11 +1,3 @@
-#
-# JSON Model Scripts
-#
-
-from .convert import schema2model_script as schema2model_script
 from .model import jmc_script as jmc_script
 
-__ALL__ = [
-    schema2model_script,
-    jmc_script,
-]
+__ALL__ = [ jmc_script ]
