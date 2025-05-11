b2746375c0cdc4ab82de0c73ae68b072f1b73cdd
Fabien Coelho <fabien@coelho.net>
2025-05-11 11:54:08 +0200

    improve generated script

--- json_model/data/python_main.py
+++ json_model/data/python_main.py
@@ -2,7 +2,20 @@
 if __name__ == "__main__":
     import json
     import sys
-    for fn in sys.argv[1:]:
+    import argparse
+    import logging
+
+    logging.basicConfig()
+    log = logging.getLogger("checker")
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--debug", "-d", action="store_true", help="set verbose mode")
+    ap.add_argument("values", nargs="*", help="JSON files")
+    args = ap.parse_args()
+
+    log.setLevel(logging.DEBUG if args.debug else logging.INFO)
+
+    for fn in args.values:
         try:
             with open(fn) as f:
                 value = json.load(f)
@@ -12,4 +25,5 @@ if __name__ == "__main__":
             else:
                 print(f"{fn}: FAIL {reasons}")
         except Exception as e:
+            log.debug(e, exc_info=args.debug)
             print(f"{fn}: ERROR ({e})")
