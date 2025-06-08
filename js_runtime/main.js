1c4c416763a6d865840aadf51a9a1c1af34f6fba
Fabien Coelho <fabien@coelho.net>
2025-06-08 09:53:55 +0200

    add performance loop with -T ...

--- js_runtime/main.js
+++ js_runtime/main.js
@@ -19,7 +19,7 @@ export default function main(checker)
 {
     const options = {
       'verbose': { type: 'boolean', short: 'v' },
-      // 'times': { type: 'string', short: 'T' },
+      'times': { type: 'string', short: 'T' },
       'report': { type: 'boolean', short: 'r' },
     }
 
@@ -27,6 +27,15 @@ export default function main(checker)
 
     // args.hasOwnProperty('verbose')
     // args.verbose === undefined
+    let times = args.values.times !== undefined ? Number(args.values.times) : 1
+
+    if (isNaN(times))
+    {
+        console.log(`Unexpected --times: ${args.values.times}`)
+        process.exit(1);
+    }
+
+    console.log(`times = ${times} ${times === Number.NaN}`)
 
     for (const fname of args.positionals)
     { 
@@ -34,6 +43,31 @@ export default function main(checker)
             fs.readFile(fname, (err, data) => {
                 if (err) throw err
                 const value = JSON.parse(data)
+
+                // show performance
+                // NOTE: jit, calls may take a lot of time at the beginning
+                // NOTE: gc, may be irregular…
+                if (times > 1)
+                {
+                    let sum = 0.0
+                    let sum2 = 0.0
+                    let n = times
+                    while (n--) {
+                        const start = performance.now()
+                        let rep = args.values.report ? [] : null
+                        const valid = checker(value, '', rep)
+                        rep && (rep.length = 0)
+                        let delay = 1000.0 * (performance.now() - start)  // µs
+                        sum += delay
+                        sum2 += delay * delay
+                    }
+                    const avg = sum / times
+                    const stdev = Math.sqrt(sum2 / times - avg * avg)
+                    console.log(`${fname}: ${args.values.report ? 'rep' : 'nop'}`,
+                                `${avg.toFixed(3)} ± ${stdev.toFixed(3)} µs`)
+                }
+
+                // do it anyway for the result
                 let rep = args.values.report ? [] : null
                 const valid = checker(value, '', rep)
                 console.log(`${fname}: ${valid ? 'PASS' : 'FAIL'}${report(rep)}`)
