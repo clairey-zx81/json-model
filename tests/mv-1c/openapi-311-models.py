c7d7a05a8aa7cf1cbd53b064b0658dac5740f88b
Fabien Coelho <fabien@coelho.net>
2025-06-10 16:33:00 +0200

    mv-1c maj + js

--- tests/mv-1c/openapi-311-models.py
+++ tests/mv-1c/openapi-311-models.py
@@ -4243,7 +4243,6 @@ def check_model_free():
         _jm_re_6_reco = None
         _jm_re_6 = None
 
-
 if __name__ == "__main__":
     check_model_init()
     main(check_model_fun, check_model_map, __version__)
