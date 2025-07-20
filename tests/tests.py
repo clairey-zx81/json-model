05ec9c1980ee8be5be09c5939283f25db1bbc127
Fabien Coelho <fabien@coelho.net>
2025-07-20 17:24:55 +0200

    fix tests.py counts

--- tests/tests.py
+++ tests/tests.py
@@ -182,8 +182,8 @@ EXPECT: dict[str, int] = {
     "mv-20:values": 112,
     "mv-20:verrors:schema": 17,
     # check 21
-    "mv-21:models": 3,
-    "mv-21:values": 34,
+    "mv-21:models": 4,
+    "mv-21:values": 46,
     "mv-21:verrors:schema": 8,
     # miscellaneous tests
     "bads:models": 57,
