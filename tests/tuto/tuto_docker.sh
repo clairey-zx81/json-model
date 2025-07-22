5833c96abe1780f458a6910b9f162cce8d6f8f46
Fabien Coelho <fabien@coelho.net>
2025-07-22 18:29:55 +0200

    add --name option to docker run

--- tests/tuto/tuto_docker.sh
+++ tests/tuto/tuto_docker.sh
@@ -4,4 +4,4 @@
 #
 
 docker pull zx80/jmc
-docker run --user $UID:$GID --rm -v .:/app/workspace -it --entrypoint ./tuto.sh zx80/jmc
+docker run --name awesome_jmc --user $UID:$GID --rm -v .:/app/workspace -it --entrypoint ./tuto.sh zx80/jmc
