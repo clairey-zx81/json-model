8fe7d2c7756b3017dc1668f2f861180aefe09f21
Fabien Coelho <fabien@coelho.net>
2025-08-12 12:32:03 +0900

    show parts while running tuto

--- tests/tuto/tuto.sh
+++ tests/tuto/tuto.sh
@@ -1,86 +1,92 @@
 #! /bin/bash
 
+echo "# jmc version"
 jmc --version
 
-# person model
+echo "# person model v0"
 jmc Person-0 hobbes.json
 jmc -r Person-0 moe.json
 
-# constraining strings
+echo "# person model v1, constraining strings"
 jmc -r Person-1 hobbes.json moe.json elysee.json
 
-# using definitions
+echo "# person model v2, using definitions"
 jmc -r Person-2 hobbes.json moe.json elysee.json
 jmc -n PI Person-2 pi.json
 
-# reusing definitions
+echo "# reusing definitions"
 jmc Class cm1.json
 
-# loosening objects
+echo "# loosening objects"
 jmc Loose moe.json
 
-# having alternatives
+echo "# having alternatives"
 jmc Geom marseille.json seoul-tokyo.json
 
-# numbers vs numbers
+echo "# numbers vs numbers"
 jmc -r Town beijing.json shanghai.json
 jmc --loose-number Town beijing.json shanghai.json
 
-# composing objects
+echo "# composing objects"
 jmc University psl.json
 
-# playing with constraints
+echo "# playing with constraints"
 jmc -r Tournament musketeers.json 3_musketeers.json
 
-# transforming models
+echo "# transforming models"
 jmc Person-3 hobbes.json bastille.json
 
-# prefering yaml or js
+echo "# prefering yaml or js"
 jmc -r Person-0.model.json hobbes.json moe.json
 jmc -r Person-0.model.yaml hobbes.json moe.json
 jmc -r Person-0.model.js hobbes.json moe.json
 
-# running with C, JS, Python, Perl, Java
-echo "# C exec"
+echo "# running with C, JS, Python, Perl, Java"
+echo "## C exec"
 jmc -o person.out Person-2
 ./person.out -T 1000000 hobbes.json
 
-echo "# JS script"
+echo "## JS script"
 jmc -o person.js Person-2
 ./person.js -T 1000000 hobbes.json
 
-echo "# Python script"
+echo "## Python script"
 jmc -o person.py Person-2
 ./person.py -T 100000 hobbes.json
 
-echo "# Perl script"
+echo "## Perl script"
 jmc -o person.pl Person-2
 [ "$PERLLIB" ] && ./person.pl -T 10000 hobbes.json
 
-echo "# Java run"
-jmc -o Person.java Person-2  # Java
+echo "## Java run"
+jmc --loose -o Person.java Person-2  # Java
 if [ "$CLASSPATH" ] ; then
   javac Person.java
   java Person -T 100000 hobbes.json
 fi
 
 # using from C, JS, Python, Perl or PL/pgSQL
+echo "# using ..."
 jmc -o person.o Person-2
 jmc -o person.mjs Person-2
 jmc -o Person.py --module Person-2
 ./test_python_module.py
+
 jmc -o PersonModels.py -E -n Root Person-2  # pydantic
 
 jmc -o person.pm Person-2
 [ "$PERLLIB" ] && PERLLIB=$PERLLIB:. ./test_perl_module.pl
 
+echo "## SQL"
 jmc -o person.sql Person-2
-psql -f person.sql
+type psql && {
+    psql -f person.sql
 
-for f in [a-z]*.json ; do
-  echo -e "$f\t$(jq -c < $f)"
-done | psql -f test_values.sql
+    for f in [a-z]*.json ; do
+      echo -e "$f\t$(jq -c < $f)"
+    done | psql -f test_values.sql
+}
 
-# exporting to, importing from JSON Schema
-jmc -E -F yaml Person-2
+echo "# exporting to, importing from JSON Schema"
+jmc --loose -E -F yaml Person-2
 jmc -E Person-2 | jsu-model
