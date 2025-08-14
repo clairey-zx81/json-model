ee476410bbaf03d5d678a178a0020358c00ce6ce
Fabien Coelho <fabien@coelho.net>
2025-08-14 21:43:44 +0900

    fix obsolete path

--- tests/mv-18/hello2.model.js
+++ tests/mv-18/hello2.model.js
@@ -1,2 +1,2 @@
 // actual github url to "hello.model.json" example
-"$https://github.com/clairey-zx81/json-model/raw/refs/heads/main/tests/modval/hello.model.json"
+"$https://github.com/clairey-zx81/json-model/raw/refs/heads/main/tests/mv-18/hello.model.json"
