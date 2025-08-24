e087d3e6142c70d2a6d7bd1eac794031e1ca24e5
Fabien Coelho <fabien@coelho.net>
2025-08-24 09:10:23 +0200

    sync file from doc

--- tests/tuto/howto_python_dyn.py
+++ tests/tuto/howto_python_dyn.py
@@ -1,15 +1,17 @@
 #! /usr/bin/env python
 import json_model as jm
 
+# model and value
+person = { "name": "/^\\w+$/", "born": "$DATE" }
 hobbes = { "name": "Hobbes", "born": "2020-07-29" }
 
 # direct model
-person = { "name": "/^\\w+$/", "born": "$DATE" }
 is_person = jm.model_checker_from_json(person)
 
 # from a URL
 is_model = jm.model_checker_from_url("https://json-model.org/models/json-model", resolver=jm.resolver.Resolver())
 
+# use model checkers on JSON values
 print("hobbes is a person:", is_person(hobbes))
 print("hobbes is a model:", is_model(hobbes))
 print("person is a person:", is_person(person))
