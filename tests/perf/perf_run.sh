e5c1515b72e108c60f2fce64ece157aa404cb326
Fabien Coelho <fabien@coelho.net>
2025-08-30 18:33:40 +0200

    filter elapsed time

--- tests/perf/perf_run.sh
+++ tests/perf/perf_run.sh
@@ -9,6 +9,11 @@ js_cli=./js-cli
 jsu_model="$jmc exec jsu-model"
 etime="/usr/bin/time -f %e"
 
+function keeptime()
+{
+    grep -E '^[0-9]+\.[0-9]+$'
+}
+
 for dir ; do
     [ -d "$dir" ] || continue
     name=${dir//-/_}
@@ -35,26 +40,32 @@ for dir ; do
     echo "## jsu-model"
     echo -n "$dir,jsu-model," >> compile.csv
     $etime $jsu_model --quiet --id --loose --no-fix $dir/schema.json 2>&1 > $dir/model.json |
-        grep -v "^WARNING:" >> compile.csv
+        keeptime >> compile.csv
     echo "## jmc-c"
     echo -n "$dir,jmc-c-src," >> compile.csv
-    $etime $jmc $jmc_c_opt -o $dir/model.c $dir/model.json 2>> compile.csv
+    $etime $jmc $jmc_c_opt -o $dir/model.c $dir/model.json 2>&1 |
+        keeptime >> compile.csv
     echo -n "$dir,jmc-c-out," >> compile.csv
-    $etime $jmc $jmc_c_opt -o $dir/model.out $dir/model.json 2>> compile.csv
+    $etime $jmc $jmc_c_opt -o $dir/model.out $dir/model.json 2>&1 |
+        keeptime >> compile.csv
     $jmc exec $dir/model.out -T $LOOP --jsonl $dir/instances.jsonl 2> ${dir}_jmc-c.out
     echo "## jmc-js"
     echo -n "$dir,jmc-js," >> compile.csv
-    $etime $jmc $jmc_x_opt -o $dir/model.js $dir/model.json 2>> compile.csv
+    $etime $jmc $jmc_x_opt -o $dir/model.js $dir/model.json 2>&1 |
+        keeptime >> compile.csv
     $jmc exec $dir/model.js -T $LOOP --jsonl $dir/instances.jsonl 2> ${dir}_jmc-js.out
     echo "## jmc-py"
     echo -n "$dir,jmc-py," >> compile.csv
-    $etime $jmc $jmc_x_opt -o $dir/model.py $dir/model.json 2>> compile.csv
+    $etime $jmc $jmc_x_opt -o $dir/model.py $dir/model.json 2>&1 |
+        keeptime >> compile.csv
     $jmc exec $dir/model.py -T $LOOP --jsonl $dir/instances.jsonl 2> ${dir}_jmc-py.out
     echo "## jmc-java-gson"
     echo -n "$dir,jmc-java-src," >> compile.csv
-    $etime $jmc $jmc_x_opt -o $dir/model.java $dir/model.json 2>> compile.csv
+    $etime $jmc $jmc_x_opt -o $dir/model.java $dir/model.json 2>&1 |
+        keeptime >> compile.csv
     echo -n "$dir,jmc-java-class," >> compile.csv
-    $etime $jmc $jmc_x_opt -o $dir/model.class $dir/model.json 2>> compile.csv
+    $etime $jmc $jmc_x_opt -o $dir/model.class $dir/model.json 2>&1 |
+        keeptime >> compile.csv
     $jmc exec java $dir/model.java -j GSON -T $LOOP --jsonl $dir/instances.jsonl 2> ${dir}_jmc-java-gson.out
     $jmc exec java $dir/model.java -j Jackson -T $LOOP --jsonl $dir/instances.jsonl 2> ${dir}_jmc-java-jackson.out
     $jmc exec java $dir/model.java -j JSONP -T $LOOP --jsonl $dir/instances.jsonl 2> ${dir}_jmc-java-jsonp.out
