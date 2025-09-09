bc83945ede6a320fa677364b75ba8f1779c55c03
Fabien Coelho <fabien@coelho.net>
2025-09-09 16:04:37 +0200

    filter out numbers

--- tests/perf/extract-to-csv.sh
+++ tests/perf/extract-to-csv.sh
@@ -3,7 +3,7 @@
 for file ; do
     name=$(basename $file .out | tr '_' ',')
     # echo "name=$name"
-    sed -e 's/[^.0-9][^.0-9]*/,/g;s/,\.,/,/g;s/,\.,/,/g;s/,$//g;s/^,//g;' $file |
+    sed -e 's/ui5/uiF/;s/cql2/cqlT/;s/draft-04/draftZF/;s/[^.0-9][^.0-9]*/,/g;s/,\.,/,/g;s/,\.,/,/g;s/,$//g;s/^,//g;' $file |
         while read line ; do
             [[ "$line" == *,* ]] && echo "$name,$line"
         done
