814496da6e15bd18506cffeb0f6b571612211c28
Fabien Coelho <fabien@coelho.net>
2025-09-10 19:03:41 +0200

    benchmarking script parallelization

--- tests/perf/benchmark.sh
+++ tests/perf/benchmark.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+# performance run in parallel
+PARALLEL=${1:-4}
+
+# performance loop count
+LOOP=${2:-1000}
+
 function err()
 {
     local status=$1
@@ -8,30 +14,63 @@ function err()
     exit $status
 }
 
+# parallelism management
+started=0 processes=""
+
+function do_start()
+{
+    echo "starting: $@"
+    "$@" &
+    processes="$processes $! "
+    let started+=1
+}
+
+function do_wait()
+{
+    if [ $started -ge $PARALLEL ] ; then
+      wait -n -p stopped $processes
+      processes=${processes/ $stopped /}
+      let started-=1
+    fi
+}
+
 echo "# sanity check"
 for cmd in git docker sqlite3 /usr/bin/time ; do
   type $cmd || err 1 "command $cmd not found"
 done
 
+for dir in jmc jsb tmp ; do
+  test -d $dir && err 2 "unexpected directory $dir"
+done
+
 echo "# cloning repos"
-git clone https://github.com/clairey-zx81/json-model.git jmc
-git clone https://github.com/sourcemeta-research/jsonschema-benchmark.git jsb
+git clone https://github.com/clairey-zx81/json-model.git jmc || err 3 "jmc clone failed"
+git clone https://github.com/sourcemeta-research/jsonschema-benchmark.git jsb || err 3 "jsb clone failed"
+mkdir tmp || err 3 "mkdir tmp failed"
 
 script_dir=$PWD/jmc/tests/perf
 export PATH=$script_dir:$PATH
 
-echo "# compilation"
-perf_run.sh 0 jsb/schemas/*
-perf_run.sh 0 jsb/schemas/*
-perf_run.sh 0 jsb/schemas/*
-perf_run.sh 0 jsb/schemas/*
+echo "# compilation runs"
+for i in 1 2 3 4 ; do
+  do_start perf_run.sh 0 tmp/${i}_ jsb/schemas/*
+  do_wait
+done
 
-echo "# run"
-perf_run.sh 1000 jsb/schemas/*
+echo "# validation runs"
+for dir in jsb/schemas/* ; do
+  do_start perf_run.sh $LOOP tmp/ $dir
+  do_wait
+done
 
 echo "# extract"
-compile-to-csv.sh *_compile.csv > compile.csv
-run-to-csv.sh *.out > perf.csv
+compile-to-csv.sh tmp/*_compile.csv > compile.csv
+run-to-csv.sh tmp/*.out > perf.csv
+
+echo "# statistics"
+echo "## number of schemas: $(echo jsb/schemas/* | wc -l)"
+echo "## number of values: $(cat jsb/schemas/*/instances.jsonl | wc -l)"
+# TODO more stats
 
 echo "# performance tables"
 sqlite3 < $script_dir/perf.sql
--- tests/perf/perf_run.sh
+++ tests/perf/perf_run.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
 export TMPDIR=.
-LOOP=$1
-shift 1
+LOOP=$1 PREFIX=$2
+shift 2
 
 # just in case
 export PATH=$PATH:.
@@ -23,8 +23,13 @@ function keeptime()
 
 for dir ; do
     [ -d "$dir" ] || continue
-    name=$(basename ${dir//-/_})
     echo "# considering $dir ($name)"
+    #
+    # Output
+    #
+    name=$(basename ${dir//-/_})
+    prefix="$PREFIX${name}"
+    compile_csv="${prefix}_compile.csv"
     #
     # Fixes
     #
@@ -38,48 +43,48 @@ for dir ; do
     # COMPILE
     #
     echo "## $dir blaze compile"
-    echo -n "$name,blaze,$now," >> ${name}_compile.csv
-    $etime $js_cli compile -m -f $dir/schema.json > $dir/blaze.json 2>> ${name}_compile.csv
+    echo -n "$name,blaze,$now," >> $compile_csv
+    $etime $js_cli compile -m -f $dir/schema.json > $dir/blaze.json 2>> $compile_csv
     blaze_ko=$?
     echo "## $dir jsu compile"
-    echo -n "$name,jsu-simpler,$now," >> ${name}_compile.csv
+    echo -n "$name,jsu-simpler,$now," >> $compile_csv
     $etime $jsu_simpler $dir/schema.json 2>&1 > $dir/schema-simpler.json |
-        keeptime >> ${name}_compile.csv
-    echo -n "$name,jsu-model,$now," >> ${name}_compile.csv
+        keeptime >> $compile_csv
+    echo -n "$name,jsu-model,$now," >> $compile_csv
     $etime $jsu_model --quiet --id --loose --no-fix $dir/schema-simpler.json 2>&1 > $dir/model.json |
-        keeptime >> ${name}_compile.csv
+        keeptime >> $compile_csv
     echo "## $dir jmc-c compile"
-    echo -n "$name,jmc-c-src,$now," >> ${name}_compile.csv
+    echo -n "$name,jmc-c-src,$now," >> $compile_csv
     $etime $jmc $jmc_c_opt -o $dir/model.c $dir/model.json 2>&1 |
-        keeptime >> ${name}_compile.csv
+        keeptime >> $compile_csv
     jmc_c_ko=$?
-    echo -n "$name,jmc-c-out,$now," >> ${name}_compile.csv
+    echo -n "$name,jmc-c-out,$now," >> $compile_csv
     $etime $jmc $jmc_c_opt -o $dir/model.out $dir/model.json 2>&1 |
-        keeptime >> ${name}_compile.csv
+        keeptime >> $compile_csv
     jmc_out_ko=$?
     echo "## $dir jmc-js compile"
-    echo -n "$name,jmc-js,$now," >> ${name}_compile.csv
+    echo -n "$name,jmc-js,$now," >> $compile_csv
     $etime $jmc $jmc_x_opt -o $dir/model.js $dir/model.json 2>&1 |
-        keeptime >> ${name}_compile.csv
+        keeptime >> $compile_csv
     jmc_js_ko=$?
     echo "## $dir jmc-py compile"
-    echo -n "$name,jmc-py,$now," >> ${name}_compile.csv
+    echo -n "$name,jmc-py,$now," >> $compile_csv
     $etime $jmc $jmc_x_opt -o $dir/model.py $dir/model.json 2>&1 |
-        keeptime >> ${name}_compile.csv
+        keeptime >> $compile_csv
     jmc_py_ko=$?
     echo "## $dir jmc-java compile"
-    echo -n "$name,jmc-java-src,$now," >> ${name}_compile.csv
+    echo -n "$name,jmc-java-src,$now," >> $compile_csv
     $etime $jmc $jmc_x_opt -o $dir/model.java $dir/model.json 2>&1 |
-        keeptime >> ${name}_compile.csv
+        keeptime >> $compile_csv
     jmc_java_ko=$?
-    echo -n "$name,jmc-java-class,$now," >> ${name}_compile.csv
+    echo -n "$name,jmc-java-class,$now," >> $compile_csv
     $etime $jmc $jmc_x_opt -o $dir/model.class $dir/model.json 2>&1 |
-        keeptime >> ${name}_compile.csv
+        keeptime >> $compile_csv
     jmc_class_ko=$?
     # echo "## $dir jmc-pl compile"
-    # echo -n "$dir,jmc-pl,$now," >> ${dir}_compile.csv
+    # echo -n "$dir,jmc-pl,$now," >> $compile_csv
     # $etime $jmc $jmc_x_opt -o $dir/model.pl $dir/model.json 2>&1 |
-    #     keeptime >> ${dir}_compile.csv
+    #     keeptime >> $compile_csv
     # jmc_pl_ko=$?
     #
     # RUN
@@ -88,30 +93,30 @@ for dir ; do
     echo "## $dir blaze run"
     [ "$blaze_ko" -eq 0 ] &&
         $js_cli validate -m $dir/blaze.json -b -l $LOOP $dir/schema.json $dir/instances.jsonl \
-            > ${name}_blaze.out
+            > ${prefix}_blaze.out
     echo "## $dir jmc-c run"
     [ "$jmc_out_ko" -eq 0 ] &&
         $jmc exec $dir/model.out -T $LOOP --jsonl $dir/instances.jsonl \
-            2> ${name}_jmc-c.out
+            2> ${prefix}_jmc-c.out
     echo "## $dir jmc-js run"
     [ "$jmc_js_ko" -eq 0 ] &&
         $jmc exec $dir/model.js -T $LOOP --jsonl $dir/instances.jsonl \
-	        2> ${name}_jmc-js.out
+	        2> ${prefix}_jmc-js.out
     echo "## $dir jmc-py run"
     [ "$jmc_py_ko" -eq 0 ] &&
         $jmc exec $dir/model.py -T $LOOP --jsonl $dir/instances.jsonl \
-            2> ${name}_jmc-py.out
+            2> ${prefix}_jmc-py.out
     echo "## $dir jmc-java run"
     [ "$jmc_class_ko" -eq 0 ] && {
         $jmc exec java $dir/model.java -j GSON -T $LOOP --jsonl $dir/instances.jsonl \
-            2> ${name}_jmc-java-gson.out
+            2> ${prefix}_jmc-java-gson.out
         $jmc exec java $dir/model.java -j Jackson -T $LOOP --jsonl $dir/instances.jsonl \
-            2> ${name}_jmc-java-jackson.out
+            2> ${prefix}_jmc-java-jackson.out
         $jmc exec java $dir/model.java -j JSONP -T $LOOP --jsonl $dir/instances.jsonl \
-            2> ${name}_jmc-java-jsonp.out
+            2> ${prefix}_jmc-java-jsonp.out
     }
     # echo "## $dir jmc-pl run"
     # [ "$jmc_pl_ko" -eq 0 ] &&
     #     $jmc exec $dir/model.pl -T $(( $LOOP / 10 )) --jsonl $dir/instances.jsonl \
-    #         2> ${dir}_jmc-pl.out
+    #         2> ${prefix}_jmc-pl.out
 done
