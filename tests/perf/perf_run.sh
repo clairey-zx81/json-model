cf6f2cce7fa67b2d3b6cf23bdf4efecabe976287
Fabien Coelho <fabien@coelho.net>
2025-09-11 09:02:14 +0200

    fix options

--- tests/perf/perf_run.sh
+++ tests/perf/perf_run.sh
@@ -38,8 +38,8 @@ for dir ; do
     jmc_c_opt="--loose --no-reporting"
     jmc_x_opt=$jmc_c_opt
     if [ $name = "cspell" -o $name = "ui5-manifest" ] ; then
-      jmc_c_opt+="-re pcre2"
-      jmc_x_opt+="-re re"
+      jmc_c_opt+=" -re pcre2"
+      jmc_x_opt+=" -re re"
     fi
     #
     # COMPILE
