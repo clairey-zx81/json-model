9e4dd792fd7951271be09ca2fc86a35616758068
Fabien Coelho <fabien@coelho.net>
2025-09-14 10:32:02 +0200

    more width adjustments

--- tests/perf/show.sql
+++ tests/perf/show.sql
@@ -5,15 +5,18 @@ SELECT COUNT(*) AS ncases, SUM(tests) AS ntests FROM Cases;
 .width -2 16 -5 -7 -6 -6 -6 -6 -6 -6 -6
 SELECT * FROM ShowPerfPerCase ORDER BY 1;
 
-.width 0
 .print # tool performance summary
+.width 0 -5 -5 -5 -5 -5 -5 -5
 SELECT * FROM ShowPerfSummary ORDER BY 1;
 
 .print # tool compile per case
+.width -2 16 -5 -5 -5 -5 -5
 SELECT * FROM ShowCompilePerCase ORDER BY 1;
 
 .print # tool compile summary
+.width 0 -5 -5 -5 -5 -5
 SELECT * FROM ShowCompileSummary ORDER BY 1;
 
 .print # result summary
+.width -2 16 -5 -5 -5 -5 -5 -5 -5
 SELECT * FROM ShowBadResults ORDER BY 1;
