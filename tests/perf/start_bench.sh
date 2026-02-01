085689d14f3363df4b141f30d2ec0bb7f7c6b5b9
Fabien Coelho <fabien@coelho.net>
2026-02-01 09:35:21 +0100

    podman-in-podman updates

--- tests/perf/start_bench.sh
+++ tests/perf/start_bench.sh
@@ -8,7 +8,8 @@ export POD=${POD:-docker}
 if [ $1 = "-h" -o $1 = "--help" ] ; then
   cat <<EOF
 
-Standalone command to start the jmc benchmark with docker-in-docker or podman-in-podman.
+Standalone command to start the jmc benchmark with docker-in-docker or podman-in-podman
+in a local directory.
 
 Arguments:
 - TAG: (docker.io/zx80/jmc-bench-$POD docker tag, defaults to "latest")
@@ -17,8 +18,8 @@ Arguments:
 Environment:
 - POD: container command, "docker" or "podman"
 - JMC_OPTS: options for jmc
-- JMC: docker.io/zx80/jmc docker tag
-- JSC: ghcr.io/sourcemeta/jsonschema container tag
+- JMC: docker.io/zx80/jmc container tag, default is "latest"
+- JSC: ghcr.io/sourcemeta/jsonschema container tag, default is "latest"
 
 Example:
 
@@ -52,16 +53,7 @@ done
 mkdir $bench_id || err 2 "cannot create directory: $bench_id"
 cd $bench_id || err 3 "cannot change directory: $bench_id"
 
-if [ "$POD" = "docker" ] ; then
-  # docker-in-docker
-  container_opts+=(-v /var/run/docker.sock:/var/run/docker.sock)
-elif [ "$POD" = "podman" ] ; then
-  # podman-in-podman
-  container_opts+=(--privileged)
-else
-  err 4 "unexpected container command: $POD"
-fi
-
+# forward environment settings to container
 if [ "$JMC_OPTS" ] ; then
   container_opts+=(-e "JMC_OPTS=$JMC_OPTS")
   bench_opts+=(--env JMC_OPTS)
@@ -75,6 +67,7 @@ if [ "$JSC" ] ; then
   bench_opts+=(--jsc "$JSC")
 fi
 
+# optional first argument container tag
 if [[ "$1" != -* ]] ; then
   bench=$1
   shift
@@ -82,9 +75,20 @@ else
   bench=latest
 fi
 
+# hackish container-in-container
 if [ "$POD" = "docker" ] ; then
-  # --user $(id -u):$(id -g)
+  # docker-in-docker
+  WORKDIR="$PWD"
+  container_opts+=(-v /var/run/docker.sock:/var/run/docker.sock)
   container_opts+=(-v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro)
+  # --user $(id -u):$(id -g)
+elif [ "$POD" = "podman" ] ; then
+  # podman-in-podman
+  WORKDIR=.
+  container_opts+=(-v $HOME/.local/share/containers:/home/podman/.local/share/containers)
+  container_opts+=(--privileged --security-opt label=disable)
+else
+  err 4 "unexpected container command: $POD"
 fi
 
 image="docker.io/zx80/jmc-bench-$POD:$bench"
@@ -98,7 +102,7 @@ $POD pull $image
 # start container
 exec $POD run --rm --name jmcbench_$bench_id \
   -v .:/workspace \
-  -e WORKDIR="$PWD" \
+  -e WORKDIR="$WORKDIR" \
   -e POD="$POD" \
   "${container_opts[@]}" \
     "$image" \
