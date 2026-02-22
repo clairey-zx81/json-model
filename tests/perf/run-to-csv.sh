9fabe83bda0d0698c9472a815bbb6d6d50bfa819
Fabien Coelho <fabien@coelho.net>
2026-02-22 09:20:50 +0100

    improve performance csv extraction script resilience

--- tests/perf/run-to-csv.sh
+++ tests/perf/run-to-csv.sh
@@ -6,8 +6,10 @@
 for file ; do
   name=$(basename $file .out | tr '_' ',')
   iter=$(echo ${file} | sed -e 's,^.*/\([0-9][0-9]*\)/[^/]*$,\1,')
-  # echo "name=$name"
-  sed -e 's/ui5/uiF/;s/cql2/cqlT/;s/draft-04/draftZF/;s/[^.0-9][^.0-9]*/,/g;s/,\.,/,/g;s/,\.,/,/g;s/,$//g;s/^,//g;' $file |
+  # echo "name=$name iter=$iter" >&2
+  sed -e 's/ui5/uiF/;s/cql2/cqlT/;s/draft-04/draftZF/;' \
+      -e 's/[^.0-9][^.0-9]*/,/g;' \
+      -e 's/,\.,/,/g;s/,\.,/,/g;s/,$//g;s/^,//g;s/^\.,//' $file |
     sed -n "/,/s/^/$name,$iter,/p" |
     grep -E "^[-a-zA-Z0-9_]*,[-a-zA-Z_]*,[0-9]+,[0-9]+,[0-9.]+,[0-9.]+,[0-9.]+$"
 done
