4bb8f9f9e705901f1f8bd39ee19a0550cced1777
Fabien Coelho <fabien@coelho.net>
2026-03-27 17:28:20 +0100

    fix cleanup

--- tests/perf/mdalign.py
+++ tests/perf/mdalign.py
@@ -17,13 +17,13 @@ ALIGN = {
 for line in fileinput.input(encoding="utf-8"):
     if re.match(r"^<!-- [CLR]+ -->$", line):
         # intercept html comment table alignment hint
-        fmt = line[5:-4]
+        fmt = line[5:-5]
     elif fmt and re.match(r"^\|([- ]+\|)+$", line):
         # ensure table alignment
         align = line.split(r"|")
+        assert len(align) > len(fmt)
         for i, f in enumerate(fmt):
             align[i+1] = ALIGN[f]
-        print("|".join(align), end="")
     else:
         # reset current format on empty line
         if line == "":
