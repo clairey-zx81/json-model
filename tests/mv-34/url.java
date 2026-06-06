d5b2d9a06684dbe9198f287d16f5119c5c120afe
Fabien Coelho <fabien@coelho.net>
2026-06-06 15:50:40 +0000

    add tests about url discrepancies, especially file://

---
+++ tests/mv-34/url.java
@@ -0,0 +1,63 @@
+
+import json_model.*;
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+@SuppressWarnings("unchecked")
+public class url extends ModelChecker
+{
+    static public final String VERSION = "2";
+
+    public Map<String, Checker> url_map_pmap;
+
+    public boolean json_model_1(Object val, Path path, Report rep)
+    {
+        return json.isString(val) && rt.is_valid_url(json.asString(val));
+    }
+
+
+    public void init(JSON json)
+    {
+        if (!initialized)
+        {
+            try {
+            url_map_pmap = new HashMap<String, Checker>();
+            url_map_pmap.put("", new Checker() { public boolean call(Object o, Path p, Report r) { return json_model_1(o, p, r);} });
+                super.init(json);
+            }
+            catch (Exception e) {
+                throw new Error("cannot initialized model checker: " + e);
+            }
+        }
+    }
+
+    public void free()
+    {
+        if (initialized)
+        {
+            super.free();
+            url_map_pmap = null;
+        }
+    }
+
+    public Checker get(String name)
+    {
+        return url_map_pmap.get(name);
+    }
+
+    public Set<String> models()
+    {
+        return url_map_pmap.keySet();
+    }
+
+    static public void main(String[] args) throws Exception
+    {
+        ModelChecker checker = new url();
+        Main.main("url", checker, VERSION, args);
+    }
+}
