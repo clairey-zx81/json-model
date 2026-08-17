dee495ca9d028619d2047eddc02a8910791673b8
Fabien Coelho <fabien@coelho.net>
2026-08-17 09:05:59 +0000

    add check and publish options

--- tests/perf/run_perf_cron.sh
+++ tests/perf/run_perf_cron.sh
@@ -2,9 +2,9 @@
 #
 # run performance script from cron
 #
- 
+
 function err()
-{ 
+{
   local status=$1
   shift 1
   echo "$@" >&2
@@ -12,14 +12,20 @@ function err()
 }
 
 # option management
-force=
+check=1 force= publish=
 
 while [[ $1 == -* ]] ; do
   opt=$1
-  shift 1
+  shift
   case $opt in
+    --check|-c) check=1 ;;
+    --no-check|-nc) check= ;;
     --force|-f) force=1 ;;
     --no-force|-nf) force= ;;
+    --publish|-p) publish=1 ;;
+    --no-publish|-np) publish= ;;
+    --id|-i) bench_id=$1 ; shift ;;
+    --id=*) bench_id=${opt#*=} ;;
     --) break ;;
     -*) err 1 "unexpected option: $opt" ;;
   esac
@@ -27,75 +33,107 @@ done
 
 # take scripts from jmc source
 PERF=$HOME/dev/json-model/tests/perf
+WORK=$HOME/dev/cron-json-model
 TARGET=$HOME/perf
 
-test -d $PERF || err 1 "missing source directory: $PERF"
-test -x $PERF/start_bench.sh || err 2 "missing executable: $PERF/start_bench.sh"
-test -d $TARGET || err 3 "missing target directory: $TARGET"
-test -d $TARGET/jsb_dir || err 4 "missing json-schema benchmark directory: $TARGET/jsb_dir"
+test -d $PERF || err 2 "missing source directory: $PERF"
+test -x $PERF/start_bench.sh || err 3 "missing executable: $PERF/start_bench.sh"
+test -d $TARGET || err 2 "missing target directory: $TARGET"
+test -d $TARGET/jsb_dir || err 2 "missing json-schema benchmark directory: $TARGET/jsb_dir"
 
-cd $TARGET || err 5 "cannot cd to: $TARGET"
+if [ "$publish" ] ; then
+  test -d $WORK || err 2 "missing working directory: $WORK"
+fi
 
-VERSION=$TARGET/.bench_version
+if [ "$check" ] ; then
+  cd $TARGET || err 5 "cannot cd to: $TARGET"
 
-# TODO also check jmc-bench image?
-SBC=ghcr.io/sourcemeta/jsonschema:latest
-JMC=docker.io/zx80/jmc:latest
+  VERSION=$TARGET/.bench_version
 
-docker pull $SBC || err 6 "cannot docker pull: $SBC"
-docker pull $JMC || err 6 "cannot docker pull: $JMC"
+  # TODO also check jmc-bench image?
+  SBC=ghcr.io/sourcemeta/jsonschema:latest
+  JMC=docker.io/zx80/jmc:latest
 
-docker run --rm $SBC --version > $VERSION.sbc.tmp || err 7 "error getting version: $SBC"
-docker run --rm --entrypoint jsu-compile $JMC --version > $VERSION.jmc.tmp || err 7 "error getting version: $JMC"
+  docker pull $SBC || err 6 "cannot docker pull: $SBC"
+  docker pull $JMC || err 6 "cannot docker pull: $JMC"
 
-# run if versions differ
-for tool in sbc jmc ; do
-  cmp -s $VERSION.$tool $VERSION.$tool.tmp || run=1
-done
+  docker run --rm $SBC --version > $VERSION.sbc.tmp || err 7 "error getting version: $SBC"
+  docker run --rm --entrypoint jsu-compile $JMC --version > $VERSION.jmc.tmp || err 7 "error getting version: $JMC"
 
-[ "$run" -o "$force" ] || err 0 "no new version, no force: skipping benchmark run"
+  # run if versions differ
+  for tool in sbc jmc ; do
+    cmp -s $VERSION.$tool $VERSION.$tool.tmp || run=1
+  done
+fi
 
-# setup standard run
-export JMC=main
-export JMC_OPTS="--single-line-regex --cc=clang --precompiled --short-version"
-# defaults are the next with 2 exceptions
-# export JSU_OPTS="--id --fix --no-strict"
-# export JSU_OPTS="--id --no-fix --no-strict"
-# export JSU_OPTS="--no-id --no-fix --no-strict"
-export JSB_DIR="$HOME/perf/jsb_dir"
-export POD_PULL=0
+if [ "$run" -o "$force" ] ; then
+  # setup standard run
 
-jmc_bench=main
+  export JMC=main
+  export JMC_OPTS="--single-line-regex --cc=clang --precompiled --short-version"
+  # defaults are the next with 2 exceptions
+  # export JSU_OPTS="--id --fix --no-strict"
+  # export JSU_OPTS="--id --no-fix --no-strict"
+  # export JSU_OPTS="--no-id --no-fix --no-strict"
+  export JSB_DIR="$HOME/perf/jsb_dir"
+  export POD_PULL=0
 
-# generate unique bench id
-let count=0 now=$(date +%Y%m%d)
-while true ; do
-  bench_id=$(printf "$now%02x" $count)
-  [ ! -e $bench_id ] && break
-  let count+=1
-  [ $count -eq 256 ] && err 8 "cannot find bench directory name"
-done
+  jmc_bench=main
 
-PARA=12 LOOP=1000 RUNS=11
+  if [ ! "$bench_id" ] ; then
+    # generate unique bench id
+    let count=0 now=$(date +%Y%m%d)
+    while true ; do
+      bench_id=$(printf "$now%02x" $count)
+      [ ! -e $bench_id ] && break
+      let count+=1
+      [ $count -eq 256 ] && err 8 "cannot find bench directory name"
+    done
+  else
+    echo "using bench_id: $bench_id"
+  fi
 
-$PERF/calcutta.sh on
-$PERF/start_bench.sh $jmc_bench $bench_id -p $PARA -l $LOOP -r $RUNS -L -c "$@"
-$PERF/calcutta.sh off
+  PARA=12 LOOP=1000 RUNS=11
 
-# test generated directory
-test -d $bench_id || err 9 "missing bench directory: $bench_id"
-for file in $bench_id/$bench_id.md $bench_id/$bench_id.json ; do
-  test -f $file || err 10 "missing generated file: $file"
-done
+  $PERF/calcutta.sh on
+  $PERF/start_bench.sh $jmc_bench $bench_id -p $PARA -l $LOOP -r $RUNS -L -c "$@"
+  $PERF/calcutta.sh off
 
-# TODO improve checks?!
+  # test generated directory
+  test -d $bench_id || err 9 "missing bench directory: $bench_id"
+  for file in $bench_id/$bench_id.md $bench_id/$bench_id.json ; do
+    test -f $file || err 9 "missing generated file: $file"
+  done
 
-# record benchmark version
-cp $VERSION.sbc.tmp $VERSION.sbc
-cp $VERSION.jmc.tmp $VERSION.jmc
-rm -f $VERSION.sbc.tmp $VERSION.jmc.tmp
+  # TODO improve run checks?!
+
+  # record benchmark version
+  if [ "$check" ] ; then
+    cp $VERSION.sbc.tmp $VERSION.sbc
+    cp $VERSION.jmc.tmp $VERSION.jmc
+    rm -f $VERSION.sbc.tmp $VERSION.jmc.tmp
+  fi
+fi
+
+if [ "$publish" ] ; then
+  # publish (new) artifact
+
+  [ "$bench_id" ] || err 12 "missing bench id for publish"
+
+  cd $WORK || err 10 "cannot cd to: $WORK"
+
+  # should be auth with a github PAT
+  git pull || err 11 "cannot git pull"
+  git checkout post || err 11 "cannot git checkout post"
+  git submodule update || err 11 "cannot git submodule update"
+
+  cp $TARGET/$bench_id/$bench_id.{json,md} $WORK/site/benchmarks/
+  git add site/benchmarks/$bench_id.* || err 11 "cannot git add artifacts"
+
+  git commit -m "add artifact $bench_id from cron job" || err 11 "cannot git commit artifacts"
+  git push || err 11 "cannot git push"
+fi
 
 # TODO
-# auto commit? pat on https clone
 # auto deploy? cron public https clone on branch post on cristalina + rsync
 # add a copy on github.io through a deployment script + project conf
