1cfb8cbf62b17a08df6da476113284f444a86902
Fabien Coelho <fabien@coelho.net>
2026-08-22 08:23:44 +0000

    improve assert counter code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,33 @@
 #
 # xdist-resistant assert counter
 #
+# count assert through a file to work around xdist multiprocess machinery
+# what hook can be used to remove the generated files is unclear
+#
+# FIXME is this reliable?
+#
 
 import pytest
 import os
+import glob
 
-# count assert through a file to work around xdist
-# probably something better can be done
 COUNTER = ".counter"
-counter = open(COUNTER, "a")
-
-def pytest_sessionstart(session):
-    global counter
+cnt: int = 0
 
 def pytest_assertion_pass(item, lineno, orig, expl):
-    # FIXME it is unclear whether data loss is possible
-    counter.write("x")
-    counter.flush()
+    global cnt
+    cnt += 1
+
+def pytest_unconfigure(config):
+    worker_id = os.environ.get("PYTEST_XDIST_WORKER", "root")
+    counter_file = f"{COUNTER}.{worker_id}"
+    with open(counter_file, "w") as cf:
+        print(cnt, file=cf)
 
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
-    n_passed_asserts = os.path.getsize(COUNTER)
-    terminalreporter.rewrite(f"\nTotal number of asserts: {n_passed_asserts}\n")
+    n_asserts: int = 0
+    files = glob.glob(f"{COUNTER}.*")
+    for cf in files:
+        with open(cf) as f:
+            n_asserts += int(f.read())
+    terminalreporter.rewrite(f"\nTotal number of asserts: {n_asserts} (over {len(files)})\n")
